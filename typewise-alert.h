9b5812e6f6c3ecc6a6b68b4626f64d88690c2ba5
gurujs44 <136423521+gurujs44@users.noreply.github.com>
2023-07-03 20:48:34 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include<map>
-using namespace std;
-
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -14,11 +11,6 @@ typedef enum {
   TOO_LOW,
   TOO_HIGH
 } BreachType;
-
-map<CoolingType,pair<int,int>> coolingTypeLimits = {
-{PASSIVE_COOLING, pair<int, int>(0,35)},
-{HI_ACTIVE_COOLING, pair<int, int>(0,45)},
-{MED_ACTIVE_COOLING, pair<int, int>(0,40)}};
 
 BreachType inferBreach(double value, double lowerLimit, double upperLimit);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
