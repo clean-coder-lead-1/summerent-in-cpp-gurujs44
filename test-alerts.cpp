2049a4b9c5f6405cb9c4a28a0a2229bd458d1eab
gurujs44 <136423521+gurujs44@users.noreply.github.com>
2023-07-03 19:42:00 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -5,40 +5,22 @@
 
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
-TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(40, 20, 30) == TOO_HIGH);
-}
-TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(25, 20, 30) == NORMAL);
 }
 
-TEST_CASE("classify and infer the breach according to limits") {
+TEST_CASE("infer the breach according to limits for PASSIVE_COOLING") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL); //0,35
-}
-TEST_CASE("classify and infer the breach according to limits") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL); //0,45
-}
-TEST_CASE("classify and infer the breach according to limits") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL); //0,40
-}
-
-TEST_CASE("classify and infer the breach according to limits") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH); //0,35
-}
-TEST_CASE("classify and infer the breach according to limits") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH); //0,45
-}
-TEST_CASE("classify and infer the breach according to limits") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45) == TOO_HIGH); //0,40
-}
-
-TEST_CASE("classify and infer the breach according to limits") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW); //0,35
 }
-TEST_CASE("classify and infer the breach according to limits") {
+TEST_CASE("infer the breach according to limits HI_ACTIVE_COOLING") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL); //0,45
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH); //0,45
   REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW); //0,45
 }
-TEST_CASE("classify and infer the breach according to limits") {
+TEST_CASE("infer the breach according to limits MED_ACTIVE_COOLING") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL); //0,40
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45) == TOO_HIGH); //0,40
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW); //0,40
 }
