bf125d98e0650ed593c9bce349476f7db95b28dc
gurujs44 <136423521+gurujs44@users.noreply.github.com>
2023-07-04 09:26:07 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -10,17 +10,59 @@ TEST_CASE("infers the breach according to limits") {
 }
 
 TEST_CASE("infer the breach according to limits for PASSIVE_COOLING") {
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL); //0,35
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH); //0,35
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW); //0,35
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
 }
-TEST_CASE("infer the breach according to limits HI_ACTIVE_COOLING") {
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL); //0,45
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH); //0,45
-  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW); //0,45
+TEST_CASE("infer the breach according to limits for HI_ACTIVE_COOLING") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 46) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -1) == TOO_LOW);
 }
-TEST_CASE("infer the breach according to limits MED_ACTIVE_COOLING") {
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL); //0,40
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45) == TOO_HIGH); //0,40
-  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW); //0,40
+TEST_CASE("infer the breach according to limits for MED_ACTIVE_COOLING") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 35) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 45) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -1) == TOO_LOW);
+}
+
+TEST_CASE("infer the breach according to limits of PASSIVE_COOLING and send alert") {
+  AlertTarget alertController = TO_CONTROLLER;
+  AlertTarget alertEmail = TO_EMAIL;
+  BatteryCharacter batteryChar;
+  
+  batteryChar.coolingType = PASSIVE_COOLING;
+  REQUIRE(checkAndAlert(alertController,batteryChar, 30) == NORMAL);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 30) == NORMAL);
+  REQUIRE(checkAndAlert(alertController,batteryChar, 40) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 40) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertController,batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, -1) == TOO_LOW);
+}
+
+TEST_CASE("infer the breach according to limits of HI_ACTIVE_COOLING and send alert") {
+  AlertTarget alertController = TO_CONTROLLER;
+  AlertTarget alertEmail = TO_EMAIL;
+  BatteryCharacter batteryChar;
+
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(alertController,batteryChar, 40) == NORMAL);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 40) == NORMAL);
+  REQUIRE(checkAndAlert(alertController,batteryChar, 46) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 46) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertController,batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, -1) == TOO_LOW);
+}
+
+TEST_CASE("infer the breach according to limits of MED_ACTIVE_COOLING and send alert") {
+  AlertTarget alertController = TO_CONTROLLER;
+  AlertTarget alertEmail = TO_EMAIL;
+  BatteryCharacter batteryChar;
+
+  batteryChar.coolingType = MED_ACTIVE_COOLING;
+  REQUIRE(checkAndAlert(alertController,batteryChar, 35) == NORMAL);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 35) == NORMAL);
+  REQUIRE(checkAndAlert(alertController,batteryChar, 45) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, 45) == TOO_HIGH);
+  REQUIRE(checkAndAlert(alertController,batteryChar, -1) == TOO_LOW);
+  REQUIRE(checkAndAlert(alertEmail,batteryChar, -1) == TOO_LOW);
 }
